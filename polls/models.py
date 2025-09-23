9e765b067f2a83e46676b2fa46b37b40199084d5
yt2b <reinventing.the.wheels@gmail.com>
2025-09-23 22:49:49 +0900

    feat: tutorial05

--- polls/models.py
+++ polls/models.py
@@ -10,7 +10,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - timezone.timedelta(days=1)
+        now = timezone.now()
+        return now - timezone.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
